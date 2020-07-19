94caf07ac2004b1536b624b9bc9a328f9d6d4ff6
abhinash.sivan <abhinashsivan@gmail.com>
2020-07-19 19:57:19 +0530

    its not working

--- FaceMask.py
+++ FaceMask.py
@@ -1,15 +1,75 @@
-import cv2
-import numpy as np
+from imutils import face_utils
 import dlib
+import cv2
+import pandas as pd
+import openpyxl
+import numpy
+import csv
+
+# let's go code an faces detector(HOG) and after detect the
+# landmarks on this detected face
+
+
+def calcDist(landmarks, image, m, n):
+    x = landmarks.part(m).x
+    y = landmarks.part(m).y
+    e = numpy.array((x, y))
+    a = landmarks.part(n).x
+    b = landmarks.part(n).y
+    o = numpy.array((a, b))
+    dist = numpy.linalg.norm(e - o)
+    return  dist
+
+# p = our pre-treined model directory, on my case, it's on the same script's diretory.
+p = "predictor_/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(p)
 
 cap = cv2.VideoCapture(0)
 
-frame = cap.read()
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("predictor_/shape_predictor_68_face_landmarks.dat")
-
+l = []
 
 while True:
-    frame=cap.read()
-    cv2.imshow("frame", frame)
+    # Getting out image by webcam
+    _, image = cap.read()
+    # Converting the image to gray scale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Get faces into webcam's image
+    rects = detector(gray, 0)
+
+    # For each detected face, find the landmark.
+    for (i, rect) in enumerate(rects):
+        # Make the prediction and transfom it to numpy array
+        landmarks = predictor(gray, rect)
+        shape = face_utils.shape_to_np(landmarks)
+
+        # Draw on our image, all the finded cordinate points (x,y)
+        for (x, y) in shape:
+            cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
+
+
+        distance_1 = calcDist(landmarks, image,38, 42)
+        distance_2 = calcDist(landmarks, image, 39, 41)
+        distance_3 = calcDist(landmarks, image, 48, 44)
+        distance_4 = calcDist(landmarks, image, 45, 47)
+
+        l.append(distance_1)
+        l.append(distance_2)
+        l.append(distance_3)
+        l.append(distance_4)
+
+
+    print (l)
+    l=[]
+    # Show the image
+    cv2.imshow("Output", image)
+
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
+
